61761cf7572ffc58131c44edbc798757f3971e1c
OriolG <download.spam.net@gmail.com>
2017-02-07 08:05:37 +0100

    added project

---
+++ AJ/AJ/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AJ.Startup))]
+namespace AJ
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
